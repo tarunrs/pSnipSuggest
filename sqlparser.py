78943aa4410420d180dd5df8f78ac16848f4c0c7
Tarun Ronur Sasikumar <tarunrs@gmail.com>
2012-05-09 01:56:45 -0400

    Added support for built in functions ISNULL and NULL

--- sqlparser.py
+++ sqlparser.py
@@ -26,6 +26,9 @@ keyword = MatchFirst((UNION, ALL, INTERSECT, EXCEPT, COLLATE, ASC, DESC, ON, USI
  COLLATE, IN, LIKE, GLOB, REGEXP, MATCH, ESCAPE, CURRENT_TIME, CURRENT_DATE, 
  CURRENT_TIMESTAMP))
  
+(AVG, MAX, BINARY_CHECKSUM, MIN, CHECKSUM, SUM, CHECKSUM_AGG, STDEV, COUNT, STDEVP, COUNT_BIG, VAR, GROUPING, VARP) = map(CaselessKeyword, """AVG, MAX, BINARY_CHECKSUM, MIN, CHECKSUM, SUM, CHECKSUM_AGG, 
+	STDEV, COUNT, STDEVP, COUNT_BIG, VAR, GROUPING, VARP""".replace(",","").split())
+aggregate_function_name = AVG | MAX | BINARY_CHECKSUM | MIN | CHECKSUM | SUM | CHECKSUM_AGG | STDEV | COUNT | STDEVP | COUNT_BIG | VAR | GROUPING | VARP
 identifier = ~keyword + Word(alphas, alphanums+"_")
 
 #columnName     = Upcase( delimitedList( identifier, ".", combine=True ) )
@@ -60,7 +63,12 @@ bind_parameter = (
     )
 type_name = oneOf("TEXT REAL INTEGER BLOB NULL")
 
-table_valued_function = function_name + LPAR + Group(Optional(delimitedList(literal_value))) + RPAR 
+table_valued_function = (Group(database_name("database") + "." + function_name) | function_name) + LPAR + Group(Optional(delimitedList(literal_value))) + RPAR 
+user_defined_function = (function_name | ISNULL | NULL) + LPAR + Group(Optional(delimitedList(column_name | literal_value))) + RPAR 
+aggregate_function = aggregate_function_name + LPAR + ("*" | column_name) + RPAR 
+#aggregate_function = (aggregate_function_name | ISNULL | NULL) + LPAR + ("*" | column_name) + RPAR 
+
+
 whereExpression = Forward()
 and_ = Keyword("and", caseless=True)
 or_ = Keyword("or", caseless=True)
@@ -85,7 +93,7 @@ whereExpression << whereCondition + ZeroOrMore( ( and_ | or_ ) + whereExpression
 
 expr_term = (
     CAST + LPAR + expr + AS + type_name + RPAR |
-    EXISTS + LPAR + select_stmt + RPAR |
+    EXISTS + LPAR + select_stmt + RPAR | column_name |
     function_name + LPAR + Optional(delimitedList(expr)) + RPAR |
     literal_value |
     bind_parameter |
@@ -123,8 +131,9 @@ single_source = ( Group((Group(database_name("database") + ".." + table_name("ta
 
 join_source << single_source + ZeroOrMore(join_op + single_source + join_constraint)
 
-result_column = Group(
-		 table_name + "." + column_name + Optional(Optional(AS) + column_alias("column_alias")) 
+result_column = Group(aggregate_function + Optional(Optional(AS) + column_alias("aggregate_function_alias")) | 
+			user_defined_function + Optional(Optional(AS) + column_alias("user_function_alias")) |
+		 column_name + Optional(Optional(AS) + column_alias("column_alias")) 
 		|"*" 
 		| table_name + "." + "*" 
 		|(expr + Optional(Optional(AS) + column_alias("column_alias")))
@@ -140,15 +149,15 @@ select_stmt << (select_core + ZeroOrMore(compound_operator + select_core) +
                 Optional(LIMIT + (integer + OFFSET + integer | integer + COMMA + integer)))
 
 tests = """\
-    SELECT c1 AS col1 FROM function1('a', 1, 2) AS t1, table12 AS t2 WHERE p.pid = p.qid group by c1, c2 order by c1 asc, c2 desc
+    SELECT c1.asd AS col1 FROM function1('a', 1, 2) AS t1, table12 AS t2 WHERE p.pid = p.qid group by c1, c2 order by c1 asc, c2 desc
 """.splitlines()
-for t in tests:
-    print t
-    try:
-	tokens = select_stmt.parseString(t)
-        print tokens.dump()
 
+
+def sql_parse(query):
+    try:
+        tokens = select_stmt.parseString(query)
+        return tokens
     except ParseException, pe:
         print pe.msg
-    print
+
 
